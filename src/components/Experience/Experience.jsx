57db0de55430337124f27240d55955d7a58721c5
Adrian <adrianhenry2115@gmail.com>
2022-04-16 09:53:55 -0400

    port, nav, xp, about jsx and css

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,12 +1,99 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-  return (
-    <section id='experience'>
-    Experience
-  </section>
-  )
-}
+ return (
+  <section id="experience">
+   <h5>What Skills I Have</h5>
+   <h2>My Experience</h2>
 
-export default Experience
+   <div className="container experience__container">
+    <div className="experience__frontend">
+     <h3>Frontend Development</h3>
+     <div className="experience__content">
+      <article className="experience__details">
+       <BsPatchCheckFill className="experience__details-icon" />
+       <div>
+        <h4>HTML</h4>
+        <small className="text-light">Experienced</small>
+       </div>
+      </article>
+      <article className="experience__details">
+       <BsPatchCheckFill className="experience__details-icon" />
+       <div>
+        <h4>CSS</h4>
+        <small className="text-light">Intermediate</small>
+       </div>
+      </article>
+      <article className="experience__details">
+       <BsPatchCheckFill className="experience__details-icon" />
+       <div>
+        <h4>Javascript</h4>
+        <small className="text-light">Experienced</small>
+       </div>
+      </article>
+      <article className="experience__details">
+       <BsPatchCheckFill className="experience__details-icon" />
+       <div>
+        <h4>Materialize</h4>
+        <small className="text-light">Novice</small>
+       </div>
+      </article>
+      <article className="experience__details">
+       <BsPatchCheckFill className="experience__details-icon" />
+       <div>
+        <h4>React</h4>
+        <small className="text-light">Competent</small>
+       </div>
+      </article>
+     </div>
+    </div>
+    {/* END OF FRONTEND */}
+
+    <div className="experience__backend">
+     <h3>Backend Development</h3>
+     <div className="experience__content">
+      <article className="experience__details">
+       <BsPatchCheckFill className="experience__details-icon" />
+       <div>
+        <h4>Node JS</h4>
+        <small className="text-light">Intermediate</small>
+       </div>
+      </article>
+      <article className="experience__details">
+       <BsPatchCheckFill className="experience__details-icon" />
+       <div>
+        <h4>MongoDB</h4>
+        <small className="text-light">Novice</small>
+       </div>
+      </article>
+      <article className="experience__details">
+       <BsPatchCheckFill className="experience__details-icon" />
+       <div>
+        <h4>MySQL</h4>
+        <small className="text-light">Advanced Beginner</small>
+       </div>
+      </article>
+      <article className="experience__details">
+       <BsPatchCheckFill className="experience__details-icon" />
+       <div>
+        <h4>Sequelize</h4>
+        <small className="text-light">intermediate</small>
+       </div>
+      </article>
+      <article className="experience__details">
+       <BsPatchCheckFill className="experience__details-icon" />
+       <div>
+        <h4>Express JS</h4>
+        <small className="text-light">Intermediate</small>
+       </div>
+      </article>
+     </div>
+    </div>
+   </div>
+  </section>
+ );
+};
+
+export default Experience;
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,12 +1,154 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/Images/port-1.png";
+import IMG2 from "../../assets/Images/port-2.png";
+import IMG3 from "../../assets/Images/port-3.png";
+import IMG4 from "../../assets/Images/port-4.png";
+import IMG5 from "../../assets/Images/port-5.png";
+import IMG6 from "../../assets/Images/port-6.png";
 
 const Portfolio = () => {
-  return (
-    <section id='portfolio'>
-     About
-   </section>
-  )
-}
+ return (
+  <section id="portfolio">
+   <h5>My Recent Work</h5>
+   <h2>Portfolio</h2>
 
-export default Portfolio
+   <div className="container portfolio__container">
+    <article className="portfolio__item">
+     <div className="portfolio__item-image">
+      <img src={IMG1} alt="port-1" />
+     </div>
+     <h3>This is a portfolio item title</h3>
+     <a
+      href="https://github.com/AdrianHenry15/blob"
+      className="btn"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Github
+     </a>
+     <a
+      href="https://rocky-wave-46848.herokuapp.com/"
+      className="btn btn-primary"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Live Demo
+     </a>
+    </article>
+    <article className="portfolio__item">
+     <div className="portfolio__item-image">
+      <img src={IMG2} alt="port-2" />
+     </div>
+     <h3>This is a portfolio item title</h3>
+     <a
+      href="https://github.com/AdrianHenry15/blob"
+      className="btn"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Github
+     </a>
+     <a
+      href="https://rocky-wave-46848.herokuapp.com/"
+      className="btn btn-primary"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Live Demo
+     </a>
+    </article>
+    <article className="portfolio__item">
+     <div className="portfolio__item-image">
+      <img src={IMG3} alt="port-3" />
+     </div>
+     <h3>This is a portfolio item title</h3>
+     <a
+      href="https://github.com/AdrianHenry15/blob"
+      className="btn"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Github
+     </a>
+     <a
+      href="https://rocky-wave-46848.herokuapp.com/"
+      className="btn btn-primary"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Live Demo
+     </a>
+    </article>
+    <article className="portfolio__item">
+     <div className="portfolio__item-image">
+      <img src={IMG4} alt="port-4" />
+     </div>
+     <h3>This is a portfolio item title</h3>
+     <a
+      href="https://github.com/AdrianHenry15/blob"
+      className="btn"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Github
+     </a>
+     <a
+      href="https://rocky-wave-46848.herokuapp.com/"
+      className="btn btn-primary"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Live Demo
+     </a>
+    </article>
+    <article className="portfolio__item">
+     <div className="portfolio__item-image">
+      <img src={IMG5} alt="port-5" />
+     </div>
+     <h3>This is a portfolio item title</h3>
+     <a
+      href="https://github.com/AdrianHenry15/blob"
+      className="btn"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Github
+     </a>
+     <a
+      href="https://rocky-wave-46848.herokuapp.com/"
+      className="btn btn-primary"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Live Demo
+     </a>
+    </article>
+    <article className="portfolio__item">
+     <div className="portfolio__item-image">
+      <img src={IMG6} alt="port-6" />
+     </div>
+     <h3>This is a portfolio item title</h3>
+     <a
+      href="https://github.com/AdrianHenry15/blob"
+      className="btn"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Github
+     </a>
+     <a
+      href="https://rocky-wave-46848.herokuapp.com/"
+      className="btn btn-primary"
+      target="_blank"
+      rel="noreferrer"
+     >
+      Live Demo
+     </a>
+    </article>
+   </div>
+  </section>
+ );
+};
+
+export default Portfolio;
