0a0408ccd7eec3dca4f88e16ae6dca7599913c6a
Adrian <adrianhenry2115@gmail.com>
2022-04-17 14:47:57 -0400

    project portolio 2 complete

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,8 +3,19 @@ import './contact.css';
 import { AiOutlineMail } from 'react-icons/ai';
 import { RiMessengerFill } from 'react-icons/ri';
 import { BsPhone } from 'react-icons/bs';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_xy138l8', 'template_qtym3op', form.current, '340q1bfh33iywOzBH');
+
+    e.target.reset();
+  };
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -13,26 +24,33 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <AiOutlineMail />
+            <AiOutlineMail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>adrianhenry2115@gmail.com</h5>
+            <h5>GMAIL</h5>
             <a href="mailto:adrianhenry215@gmail.com">Send A Message</a>
           </article>
           <article className="contact__option">
-            <RiMessengerFill />
+            <RiMessengerFill className="contact__option-icon" />
             <h4>Facebook Messenger</h4>
             <h5>Adrian Henry</h5>
             <a href="https://m.me/thirdgenerationrecords">Send Me A Direct Message</a>
           </article>
           <article className="contact__option">
-            <BsPhone />
+            <BsPhone className="contact__option-icon" />
             <h4>Phone</h4>
             <h5>(321)-370-0836</h5>
             <a href="sms:(321)(3700836)">Text Me</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action=""></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your Email" />
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,64 @@
 import React from 'react';
 import './footer.css';
+import { AiFillFacebook } from 'react-icons/ai';
+import { TiSocialInstagram } from 'react-icons/ti';
+import { AiFillLinkedin } from 'react-icons/ai';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        Adrian
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          {' '}
+          <a href="#">Home</a>
+        </li>
+        <li>
+          {' '}
+          <a href="#about">About</a>
+        </li>
+        <li>
+          {' '}
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          {' '}
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          {' '}
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          {' '}
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          {' '}
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <AiFillFacebook />
+        </a>
+        <a href="https://Instagram.com">
+          <TiSocialInstagram />
+        </a>
+        <a href="https://Linkedin.com">
+          <AiFillLinkedin />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Adrian Henry. All Rights Reserved</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
