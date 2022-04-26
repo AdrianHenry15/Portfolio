24e3d36dd1dbd4e2d1e9fae0215747c53558f983
Adrian <adrianhenry2115@gmail.com>
2022-04-26 07:26:20 -0400

    resumse update

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import Resume from '../../assets/Images/Resume 4.15.pdf';
+import Resume from '../../assets/Images/Resume 4.23.pdf';
 
 const CTA = () => {
   return (
